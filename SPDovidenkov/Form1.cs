bc8c5009f267a1515aee4ad2934358387292a8e8
Yogel9 <Dovidenkov@users.noreply.github.com>
2021-03-16 20:16:25 +0300

    Start

---
+++ SPDovidenkov/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SPDovidenkov
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox.SelectedIndex = 0;//при старте в комобобоксе 1
+        }
+
+        private void ButStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButDel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
